546456e097a66a2bd614d5e8967346bb25c2c75b
Deniss <den.b@itservicesgroup.co.uk>
2017-07-15 23:15:34 +0100

    Add Simple Graphical UI, choose menu
    Check menu for availabe options
    run ./lib/coolpay_api/authentication.rb

--- lib/coolpay_api/authentication.rb
+++ lib/coolpay_api/authentication.rb
@@ -3,6 +3,7 @@
 rubygems if RUBY_VERSION < '1.9'
 require 'rest_client'
 require_relative 'recipient'
+require_relative 'payment'
 require 'active_support/all'
 
 module Authentication
@@ -42,4 +43,40 @@ module Authentication
   class MissingApiKey < StandardError ; end
 end
 
+unless ENV['TEST'] == 'test'
+  token = Authentication.authenticate
+  recipient = Recipient.new(token)
+  payment = Payment.new(token)
+  puts " \t Welcome to the payment terminal\n "
+  puts " \t Please choose letter [t,l,c,r]:"
 
+  puts " \t #########################"
+  puts " \t # t: create recipient"
+  puts " \t # l: list recipients"
+  puts " \t # r: create payment"
+  puts " \t # c: check payment status"
+  puts " \t # x: exit"
+  puts " \t #########################"
+  input = gets.chomp
+
+  case input
+  when 't'
+    puts "Create recipient please enter name:"
+    user_name = gets.chomp
+    recipient.create(user_name)
+  when 'l'
+    puts recipient.list
+  when 'c'
+    puts "Check what paid to user, enter user_id:"
+    user_id = gets.chomp
+    puts payment.is_paid_to?(user_id)
+  when 'r'
+    puts "Please enter user id:"
+    user_id = gets.chomp
+    puts "Please enter amount:"
+    paid_amount = gets.chomp
+    payment.create(user_id, paid_amount)
+  when 'x'
+    puts "Thank you for using payment terminal"
+  end
+end
